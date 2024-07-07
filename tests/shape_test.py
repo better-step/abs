d9c5d0341474753c77c0662c07c2c3b57dec57b4
Nafiseh <90077573+Nafiseh-Izadyar@users.noreply.github.com>
2024-07-07 14:35:28 -0700

    updated shape test

--- tests/shape_test.py
+++ tests/shape_test.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import h5py
 import os
 from abs.shape import Shape
+from abs.no_name import *
+
 
 def read_file(file_path):
     assert Path(file_path).exists(), "Please provide valid file path"
@@ -12,11 +14,15 @@ def get_file(sample_name):
     return os.path.abspath(os.path.join(os.getcwd(), '..', 'abs', 'data', 'sample_hdf5', sample_name))
 
 
+def l_function(shape, geo, points):
+    return geo.normal(points)
+
+
 sample_name = 'cylinder_Hole.hdf5'
 file_path = get_file(sample_name)
 with h5py.File(file_path, 'r') as hdf:
     geo = hdf['geometry/parts']
     topo = hdf['topology/parts']
-    shape = Shape(geo, topo)
-    print(shape)
+    s = Shape(geo, topo)
 
+get_data(s, 10, l_function)
