448bc15d1275487f5ce3338886af176df1394088
Teseo Schneider <teseo@bluewin.ch>
2024-07-09 10:30:57 -0700

    add pts to get data

--- abs/no_name.py
+++ abs/no_name.py
@@ -4,20 +4,28 @@ from abs.sampling import surface_sampler
 
 def get_data(shape, num_samples, lambda_func):
     ss = []
+    pts = []
     for curve2d in shape.Geometry._curves2d:
-        uv_points, _ = curve_sampler.uniform_parametric_sample(curve2d, 0.01)
+        uv_points, pt = curve_sampler.uniform_parametric_sample(curve2d, 0.01)
         s = lambda_func(shape, curve2d, uv_points)
         ss.append(s)
+        pts.append(pt)
 
     for curve3d in shape.Geometry._curves3d:
-        uv_points, _ = curve_sampler.uniform_parametric_sample(curve3d, 0.01)
+        uv_points, pt = curve_sampler.uniform_parametric_sample(curve3d, 0.01)
         s = lambda_func(shape, curve3d, uv_points)
         ss.append(s)
+        pts.append(pt)
 
     for surface in shape.Geometry._surfaces:
-        uv_points, _ = surface_sampler.uniform_parametric_sample(surface, 0.01)
+        uv_points, pt = surface_sampler.uniform_parametric_sample(surface, 0.01)
         s = lambda_func(shape, surface, uv_points)
         ss.append(s)
+        pts.append(pt)
+
+
+    return pts, ss
+
 
 
 
