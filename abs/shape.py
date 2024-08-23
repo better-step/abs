d02cfca5d22ba57b13f2707fa2963b3f83da8c4f
teseoch <teseo.schneider@gmail.com>
2024-08-23 14:31:42 -0700

    fixed trimming for parts

--- abs/shape.py
+++ abs/shape.py
@@ -72,46 +72,53 @@ class Shape:
         self._2d_trimming_curves = []
 
         for _, part in enumerate(self.Topology._topology):
-            for _, solid in enumerate(part.solids):
-                for shell_index in solid['shells']:
-                    shell = part.shells[shell_index]
+            if len(part.solids) == 0:
+                for shell_index, _ in enumerate(part.shells):
+                    self._process_2d_trimming_curves_for_shell(part, shell_index, curves2d, curves3d, spacing)
+            else:
+                for _, solid in enumerate(part.solids):
+                    for shell_index in solid['shells']:
+                        self._process_2d_trimming_curves_for_shell(part, shell_index, curves2d, curves3d, spacing)
 
-                    for (face_index, _) in shell['faces']:
-                        face = part.faces[face_index]
-                        self._2d_trimming_curves += (face_index-len(self._2d_trimming_curves)+1)* [None]
-                        self._2d_trimming_curves[face_index] = []
+    def _process_2d_trimming_curves_for_shell(self, part, shell_index, curves2d, curves3d, spacing):
+        shell = part.shells[shell_index]
 
-                        surface_index = face['surface']
-                        surface = self.Geometry._surfaces[surface_index]
+        for (face_index, _) in shell['faces']:
+            face = part.faces[face_index]
+            self._2d_trimming_curves += (face_index-len(self._2d_trimming_curves)+1)* [None]
+            self._2d_trimming_curves[face_index] = []
 
-                        for loop_id in face['loops']:
-                            loop = part.loops[loop_id]
-                            for halfedge_index in loop['halfedges']:
-                                halfedge = part.halfedges[halfedge_index]
-                                modified_orientation = part.determine_curve_orientation(face_index, halfedge_index)
+            surface_index = face['surface']
+            surface = self.Geometry._surfaces[surface_index]
 
-                                if '2dcurve' in halfedge:
-                                    curve2d_index = halfedge['2dcurve']
-                                    curve2d = curves2d[curve2d_index]
-                                    _, closest_surface_uv_values_of_curve = curve_sampler.uniform_sample(curve2d, spacing, 4, 300)
-                                    if not modified_orientation:
-                                        closest_surface_uv_values_of_curve = closest_surface_uv_values_of_curve[::-1]
-                                else:
-                                    surface_uv_values, surface_points = surface_sampler.uniform_sample(surface, spacing)
+            for loop_id in face['loops']:
+                loop = part.loops[loop_id]
+                for halfedge_index in loop['halfedges']:
+                    halfedge = part.halfedges[halfedge_index]
+                    modified_orientation = part.determine_curve_orientation(face_index, halfedge_index)
 
-                                    curve3d_index = halfedge['edge']
-                                    curve3d = curves3d[curve3d_index]
+                    if '2dcurve' in halfedge:
+                        curve2d_index = halfedge['2dcurve']
+                        curve2d = curves2d[curve2d_index]
+                        _, closest_surface_uv_values_of_curve = curve_sampler.uniform_sample(curve2d, spacing, 4, 300)
+                        if not modified_orientation:
+                            closest_surface_uv_values_of_curve = closest_surface_uv_values_of_curve[::-1]
+                    else:
+                        surface_uv_values, surface_points = surface_sampler.uniform_sample(surface, spacing)
+
+                        curve3d_index = halfedge['edge']
+                        curve3d = curves3d[curve3d_index]
 
 
-                                    # Sample the curve points to get UV values
-                                    _, curve_points = curve_sampler.uniform_sample(curve3d, spacing)
+                        # Sample the curve points to get UV values
+                        _, curve_points = curve_sampler.uniform_sample(curve3d, spacing)
 
-                                    if not modified_orientation:
-                                        curve_points = curve_points[::-1]
-                                    # Calculate the nearest UV values on the surface for the curve points
-                                    closest_surface_uv_values_of_curve = find_surface_uv_for_curve(surface_points, surface_uv_values, curve_points)
+                        if not modified_orientation:
+                            curve_points = curve_points[::-1]
+                        # Calculate the nearest UV values on the surface for the curve points
+                        closest_surface_uv_values_of_curve = find_surface_uv_for_curve(surface_points, surface_uv_values, curve_points)
 
-                                self._2d_trimming_curves[face_index].append(closest_surface_uv_values_of_curve)
+                    self._2d_trimming_curves[face_index].append(closest_surface_uv_values_of_curve)
 
 
     class Geometry:
