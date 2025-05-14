6bbbc11b2fe0e33b5ef5b527411fe7def5034c5c
Sai Chandra <20564967+itsmeeChandU@users.noreply.github.com>
2025-05-13 21:20:49 -0700

    updated version

--- abs/__init__.py
+++ abs/__init__.py
@@ -33,7 +33,7 @@ except ModuleNotFoundError as exc:
         stacklevel=2,
     )
 except PackageNotFoundError:
-    __version__ = "0.1.0"
+    __version__ = "0.1.1"
 
 __all__ = [
     "read_parts",
