1a17e8e194f76567ade51729b5c97775f3028a21
teseoch <teseo.schneider@gmail.com>
2025-12-10 10:15:42 -0800

    disabling mshio from nanospline

--- cpp/nanospline.cmake
+++ cpp/nanospline.cmake
@@ -7,10 +7,11 @@ message(STATUS "Third-party (external): creating target 'nanospline::nanospline'
 include(CPM)
 CPMAddPackage(
     NAME nanospline
-    GITHUB_REPOSITORY qnzhou/nanospline
-    GIT_TAG        5657b8d39178e81ebf9ac91b37b47516ec361185
+    GITHUB_REPOSITORY teseoch/nanospline
+    GIT_TAG        a4bba801c09177f2beaf0522554e4fdf7a49ce1a
     OPTIONS
     "NANOSPLINE_BUILD_TESTS Off"
+    "NANOSPLINE_MSHIO Off"
 )
 FetchContent_MakeAvailable(nanospline)
 
